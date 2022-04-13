9bb42102e2c9d2e273ba6bc3ca805d51ef07a08a
David Attali <davidattali@Davids-MacBook-Air.local>
2022-04-13 15:50:59 -0500

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ let helmet = require('helmet')
 var app = express();
 
 const mongoose = require('mongoose')
-const mongoDB = "mongodb+srv://dattali:awesome@cluster0.tiy9z.mongodb.net/Catalogue?retryWrites=true&w=majority"
+const dev_db_url = "mongodb+srv://dattali:awesome@cluster0.tiy9z.mongodb.net/Catalogue?retryWrites=true&w=majority"
+const mongoDB = process.env.MONGODB_URI || dev_db_url
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
